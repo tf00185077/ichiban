cfe13862edad546297dff91a6135eb7b96931ae0
redgodkill <tf00185077@gmail.com>
2024-05-09 18:04:24 +0800

    add ts

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 const Header = () => {
     return <div className="text-3xl font-bold underline">Header</div>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from '@/components/header/Header.jsx'
+import Header from '@/components/header/Header.tsx'
 import {
   RouterProvider
 } from "react-router-dom";
