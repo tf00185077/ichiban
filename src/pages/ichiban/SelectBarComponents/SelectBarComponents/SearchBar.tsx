c7fd4b53a649e2f98e9010af3d4a3674455882af
ryanhsuyo <chiayuhsu7936@gmail.com>
2024-05-14 21:31:06 +0800

    ichiban: add card component

--- src/pages/ichiban/SelectBarComponents/SelectBarComponents/SearchBar.tsx
+++ src/pages/ichiban/SelectBarComponents/SelectBarComponents/SearchBar.tsx
@@ -6,12 +6,15 @@ export const SearchBar = () => {
         dispatch({type: 'UPDATE_KEYWORD', payload: event.target.value})
     }
     const search = () => {
-        
+        console.log(state,'搜尋的東西')
     }
     return (
         <>
-           <input onChange={handleChange} value={state.searchKeyword}></input> 
-           <button onClick={search}>搜尋</button>
+            <div className="bg-white flex text-yellow-500 w-full  ring-1 ring-slate-700 rounded-full py-1 px-2">
+                <input onChange={handleChange} className="p-2d" placeholder="搜尋關鍵字" value={state.searchKeyword} ></input> 
+                
+                <button onClick={search}  className="bg-black text-yellow-500 w-full ring-2 ring-amber-400 rounded-2xl px-4" >搜尋</button>
+            </div>
         </>
     )
 }
