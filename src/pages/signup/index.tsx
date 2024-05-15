77d6d1823a618a600b07491884a3e1929904b49b
tf00185077 <tf00185077@gmail.com>
2024-05-16 01:40:49 +0800

    signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,20 @@
+import MainCard from "@/components/layout/MainCard";
+import SignupForm from "@/components/form/SignupForm";
 const Singup = () => {
-    return<>
-        <div>signup</div>
-    </>
-}
+  return (
+    <MainCard>
+      <div className="mb-15">首頁/盲盒</div>
+      <div className="mt-20 mx-auto max-w-[700px] flex flex-col">
+        <p className="text-center">
+          為避免您的賞品寄送權益，請務必如實填寫會員資料
+        </p>
+        <p className="text-center mb-[20px]">
+          <span className="text-red-600">*</span>為必填項目
+        </p>
+        <SignupForm></SignupForm>
+      </div>
+    </MainCard>
+  );
+};
 
-export default Singup
+export default Singup;
