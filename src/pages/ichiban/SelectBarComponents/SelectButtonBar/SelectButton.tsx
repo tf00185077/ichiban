d6751d863c38f84e27c7815454e05145f327aa85
ryanhsuyo <chiayuhsu7936@gmail.com>
2024-05-16 11:20:10 +0800

    ichiban:  SelectButton remove annotation

--- src/pages/ichiban/SelectBarComponents/SelectButtonBar/SelectButton.tsx
+++ src/pages/ichiban/SelectBarComponents/SelectButtonBar/SelectButton.tsx
@@ -1,7 +1,6 @@
 import { SelectButtonListProps } from './type'
 
 export const SelectButton = ({ item, index }: { item: SelectButtonListProps, index: number }) => {
-    // ${item.selected ? 'bg-yellow-500 text-white' : 'text-brown-800'
     return (
         <div className=''>
             <button className={` text-yellow-900 text-lg font-bold px-4 py-2 rounded-full border-[3px] border-yellow-900 hover:cursor-pointer hover:opacity-70}`}>
