e2029f1a7ebdb20dbaa3bc07e79eaa7b1cd17f15
ryanhsuyo <chiayuhsu7936@gmail.com>
2024-05-16 09:52:10 +0800

    ichiban: new card

--- src/pages/ichiban/SelectBarComponents/SelectBarComponents/SelectManufacturer.tsx
+++ src/pages/ichiban/SelectBarComponents/SelectBarComponents/SelectManufacturer.tsx
@@ -10,7 +10,11 @@ export const SelectManufacturer = () => {
     };
     return (
         <>
-            <select className="bg-black text-yellow-500 w-full" onChange={handleChange} value={state.selectedManufacturer}>
+            <select
+                className="bg-black text-yellow-500 w-full hover:opacity-70 rounded-full py-2.5 px-4 ring-4 ring-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-500 cursor-pointer"
+                onChange={handleChange} 
+                value={state.selectedManufacturer}
+            >
                 <option value="">全部產品</option>
                 {ManufacturerLists.map((item, index)=>(
                     <option key={index} value={item.name_en}>{item.name_tw}</option>
