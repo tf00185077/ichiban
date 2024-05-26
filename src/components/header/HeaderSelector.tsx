2e8b8017d374c1ab9b6e9229b6ae8d98c1cc322e
tf00185077 <tf00185077@gmail.com>
2024-05-27 00:56:58 +0800

    fix css and fix pic src

--- src/components/header/HeaderSelector.tsx
+++ src/components/header/HeaderSelector.tsx
@@ -17,7 +17,7 @@ const HeaderSelector = ({ name,iconName }: { name: keyof typeof routerName,iconN
         className="flex items-center h-[35px] mr-[15px] text-white hover:text-[#f4c221] text-[17.6px] font-bold font-header px-[10px] bg-[#23058C80] hover:bg-[#070137E6] text-shadow rounded-[30px]"
         to={`${routerName[name]}`}
           >
-            <img className="w-[25px] h-[25px]" src={`src/assets/icon/${iconName}.png`} alt="" />
+            <img className="w-[25px] h-[25px]" src={`/src/assets/icon/${iconName}.png`} alt="" />
         {name}
       </Link>
     </>
--- src/components/layout/MainCard.tsx
+++ src/components/layout/MainCard.tsx
@@ -1,6 +1,6 @@
 const MainCard =  ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="w-4/5 max-w-[1400px] mx-auto h-full bg-white p-[30px]">
+        <div className="w-4/5 max-w-[1400px] mx-auto h-full bg-white p-[30px] font-header">
             {children}
         </div>
     )
